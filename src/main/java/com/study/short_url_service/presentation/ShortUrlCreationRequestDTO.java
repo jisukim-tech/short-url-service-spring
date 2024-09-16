c7eb416f8cadc5b78cee8e1338f23a03395b3cbf
jisukimtech <jisukim.tech@gmail.com>
2024-09-16 21:15:31 +0900

    test: URL 조회에 대한 통합 테스트 추가
    - 기존 URL을 단축한 후 단축된 URL key로 기존 URL을 조회할 수 있는지 테스트
    - 존재하지 않는 단축된 URL key로 기존 URL을 조회하는 경우 예외가 발생하는지 테스트
    - 존재하지 않는 단축된 URL key로 단축 URL 정보를 조회하는 경우 예외가 발생하는지 테스트

--- src/main/java/com/study/short_url_service/presentation/ShortUrlCreationRequestDTO.java
+++ src/main/java/com/study/short_url_service/presentation/ShortUrlCreationRequestDTO.java
@@ -8,6 +8,10 @@ public class ShortUrlCreationRequestDTO {
     @URL(regexp = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)")
     private String originalUrl;
 
+    public ShortUrlCreationRequestDTO(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
     public String getOriginalUrl() {
         return originalUrl;
     }
